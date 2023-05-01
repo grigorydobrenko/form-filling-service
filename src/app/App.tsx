43c7793cd3fb93053c7fc07748ae7b4058621786
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-05-01 22:29:12 +0300

    feat: add pick gender and age

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './App.module.scss';
 import SideBar from "../layouts/sidebar/SideBar";
-import CommonPage from "../pages/common_page/CommonPage";
+import CommonPage from "../pages/CommonPage/CommonPage";
 
 function App() {
     return (
--- src/pages/CommonPage/CommonPage.tsx
+++ src/pages/CommonPage/CommonPage.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as Logo} from "../../assets/user.svg";
+import {ReactComponent as Man} from "../../assets/m.svg";
+import {ReactComponent as Woman} from "../../assets/f.svg";
 
 import style from './CommonPage.module.scss'
 import {Input} from "../../components/Input/Input";
@@ -7,20 +9,25 @@ import {Label} from "../../components/Label/Label";
 import {Select} from "../../components/Select/Select";
 import {IOption} from "../../components/Select/Select.props";
 import {Button} from "../../components/Button/Button";
+import cn from "classnames";
 
 export const CITY_DATA: IOption[] = [
-    { label: 'Санкт-Птеребург', value: 'Санкт-Птеребург' },
-    { label: 'Москва', value: 'Москва' },
-    { label: 'Минск', value: 'Минск' },
+    {label: 'Санкт-Птеребург', value: 'Санкт-Птеребург'},
+    {label: 'Москва', value: 'Москва'},
+    {label: 'Минск', value: 'Минск'},
 ];
 
 export const COUNTRY: IOption[] = [
-    { label: 'Россия', value: 'Россия' },
-    { label: 'Беларусь', value: 'Беларусь' },
-    { label: 'Казахстан', value: 'Казахстан' },
+    {label: 'Россия', value: 'Россия'},
+    {label: 'Беларусь', value: 'Беларусь'},
+    {label: 'Казахстан', value: 'Казахстан'},
 ];
 
 const CommonPage = (): JSX.Element => {
+
+    const initialState = 'male'
+    const [gender, setGender] = useState(initialState)
+
 
     return (
         <div className={style.container}>
@@ -58,10 +65,25 @@ const CommonPage = (): JSX.Element => {
                     <Select options={COUNTRY}/>
                 </Label>
                 <div>
-                    Пол
+                    <Label label={'Пол*'} className={style.genders}>
+                        <Button color={'white'}
+                                className={cn(style.gender_btn, {[style.gender_btn_active]: gender === 'male'})}
+                                onClick={() => setGender('male')}>
+                            <div className={cn(style.inner, {[style.inner_active]: gender === 'male'})}>
+                                <Man/><span>М</span></div>
+                        </Button>
+                        <Button color={'white'}
+                                className={cn(style.gender_btn, {[style.gender_btn_active]: gender === 'female'})}
+                                onClick={() => setGender('female')}>
+                            <div className={cn(style.inner, {[style.inner_active]: gender === 'female'})}>
+                                <Woman/><span>Ж</span></div>
+                        </Button>
+                    </Label>
                 </div>
                 <div>
-                    Дата рождения
+                    <Label label={'Дата рождения*'}>
+                        <input type="date" className={style.date}/>
+                    </Label>
                 </div>
             </div>
             <div className={style.row}>
@@ -70,8 +92,8 @@ const CommonPage = (): JSX.Element => {
                 </Label>
             </div>
             <div className={style.form_buttons}>
-                <Button className={style.button} color={'white'}>Отмена</Button>
-                <Button className={style.button}>Далее</Button>
+                <Button color={'white'}>Отмена</Button>
+                <Button>Далее</Button>
             </div>
         </div>
     );
