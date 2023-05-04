2c4626454a96e7b9d81120accd708073952be42a
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-05-04 22:30:46 +0300

    feat: add validation LLC Page

--- src/pages/OnwershipPage/LLCPage/LLCForm/LLCForm.tsx
+++ src/pages/OnwershipPage/LLCPage/LLCForm/LLCForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Label} from "../../../../components/Label/Label";
-import style from "../../EntrepreneurPage/EntrepreneurForm/EntrepreneurForm.module.scss";
+import style from "./LLCForm.module.scss";
 import {Select} from "../../../../components/Select/Select";
 import {Input} from "../../../../components/Input/Input";
 import {InputWithMask} from "../../../../components/InputWithMask/InputWithMask";
@@ -9,6 +9,7 @@ import useOwnershipForm from "../../../../hooks/useOwnershipForm";
 import {useSetOwnerShip} from "../../../../hooks/useSetOwnerShip";
 import {OWNERSHIP} from "../../OwnershipPage";
 import {useAppSelector} from "../../../../app/hooks";
+import InputDropzone from "../../../../components/Dropzone/Dropzone";
 
 const LlCForm = (): JSX.Element => {
 
@@ -21,7 +22,6 @@ const LlCForm = (): JSX.Element => {
         setValue,
         onSubmit,
         validationScheme,
-        dispatch
     } = useOwnershipForm()
 
     const {setOwnerShip} = useSetOwnerShip()
@@ -39,6 +39,7 @@ const LlCForm = (): JSX.Element => {
                 <Label label={'Наименование полное*'}>
                     <Input
                         placeholder={'ООО «Московская промышленная компания»'}
+                        {...register("name", validationScheme)}
                     />
                     {errors.name && <span className={style.red}>{errors.name.message}</span>}
                 </Label>
@@ -53,32 +54,35 @@ const LlCForm = (): JSX.Element => {
             <div className={style.row}>
                 <Label label={'Дата регистрации*'} className={style.row_inner}>
                     <InputWithMask mask='99.99.9999' maskPlaceholder='дд.мм.гггг'
-                                   placeholder={'дд.мм.гггг'}/>
-                    {/*{errors.registrationDate && <span className={style.red}>{errors.registrationDate.message}</span>}*/}
+                                   placeholder={'дд.мм.гггг'}
+                                   {...register('registrationDate', validationScheme)}
+                    />
+                    {errors.registrationDate && <span className={style.red}>{errors.registrationDate.message}</span>}
                 </Label>
                 <Label label={'ИНН*'} className={style.row_inner}>
                     <InputWithMask mask='99999999' maskPlaceholder='х'
                                    placeholder={'xxxxxxxx'}
+                                   {...register('inn', validationScheme)}
                     />
-                    {/*{errors.inn && <span className={style.red}>{errors.inn.message}</span>}*/}
+                    {errors.inn && <span className={style.red}>{errors.inn.message}</span>}
                 </Label>
                 <Label label={'Скан ИНН*'}>
-                    {/*<InputDropzone*/}
-                    {/*    // setValue={setValue} name={'innImg'} setError={setError} clearErrors={clearErrors}*/}
-                    {/*/>*/}
-                    {/*{errors.innImg && <span className={style.red}>{errors.innImg.message}</span>}*/}
+                    <InputDropzone
+                        setValue={setValue} name={'innImg'} setError={setError} clearErrors={clearErrors}
+                    />
+                    {errors.innImg && <span className={style.red}>{errors.innImg.message}</span>}
                 </Label>
             </div>
             <div className={style.row}>
                 <Label label={'ОГРН*'}>
                     <InputWithMask mask='999999999999999' maskPlaceholder='х'
                                    placeholder={'ххххххххххххххх'}
+                                   {...register('ogrnip', validationScheme)}
                     />
-                    {/*{errors.ogrnip && <span className={style.red}>{errors.ogrnip.message}</span>}*/}
+                    {errors.ogrnip && <span className={style.red}>{errors.ogrnip.message}</span>}
                 </Label>
-
             </div>
-            <div className={style.row}>
+            <div className={style.llc_buttons}>
                 <Button color={'white'}>Назад</Button>
                 <Button type={'submit'}>Далее</Button>
             </div>
